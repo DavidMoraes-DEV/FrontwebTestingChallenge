0d178b8eb994ab3ff0ebc1d98cebccad5edb1cbe
David Moraes de Oliveira <DavidMoraes-DEV@outlook.com>
2022-11-07 18:27:30 -0300

    Solution for EmployeeList tests

--- frontweb/src/pages/Admin/Employees/List/index.tsx
+++ frontweb/src/pages/Admin/Employees/List/index.tsx
@@ -3,39 +3,69 @@ import './styles.css';
 import Pagination from 'components/Pagination';
 import EmployeeCard from 'components/EmployeeCard';
 import { Link } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import { Employee } from 'types/employee';
+import { AxiosRequestConfig } from 'axios';
+import { requestBackend } from 'util/requests';
+import { hasAnyRoles } from 'util/auth';
 
-const employeeHardCode = { // delete
-  id: 1,
-  name: "Carlos",
-  email: "carlos@gmail.com",
-  department: {
-    id: 1,
-    name: "Sales"
-  }
+type ControlComponentsData = {
+  activePage: number;
 };
 
 const List = () => {
 
+  const [page, setPage] = useState<SpringPage<Employee>>();
+  
+  const [controlComponentsData, setControlComponentsData] =
+  useState<ControlComponentsData>({
+    activePage: 0,
+  });
+
+  const getUsers = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: "/employees",
+      withCredentials: true,
+      params: {
+        page: controlComponentsData.activePage,
+        size: 4,
+      }
+    };
+
+    requestBackend(config)
+    .then((response) => {
+      setPage(response.data);
+    });
+
+  }, [controlComponentsData]);
+
   const handlePageChange = (pageNumber: number) => {
-    // to do
+    setControlComponentsData({ activePage: pageNumber });
   };
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
 
   return (
     <>
-      <Link to="/admin/employees/create">
-        <button className="btn btn-primary text-white btn-crud-add">
-          ADICIONAR
-        </button>
-      </Link>
+      {hasAnyRoles(['ROLE_ADMIN']) && (
+        <Link to="/admin/employees/create">
+          <button className="btn btn-primary text-white btn-crud-add">
+            ADICIONAR
+          </button>
+        </Link>
+      )}
 
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
-      <EmployeeCard employee={employeeHardCode} />
+      {page?.content.map(employee => (
+        <EmployeeCard employee={employee} key={employee.id} />
+      ))}
 
       <Pagination
-        forcePage={0}
-        pageCount={1}
+        forcePage={page?.number}
+        pageCount={page ? page.totalPages : 0}
         range={3}
         onChange={handlePageChange}
       />
